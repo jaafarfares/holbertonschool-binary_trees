28d0d454107d99303fa12a8c4ca7f3846ac24e45
jaaferfares <jaafarferes@gmail.com>
2022-07-19 12:30:47 +0100

    push all

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,27 +1,27 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_insert_left - ......
+ *@parent: .........
+ *@value: ......
+ *Return: ...
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node_n;
+	binary_tree_t *left_n;
 
-	node_n = binary_tree_node(parent, value);
-	
-	if (node_n == NULL || parent == NULL)
+	left_n = binary_tree_node(parent, value);
+	if (left_n == NULL || parent == NULL)
 		return (NULL);
 
-	node_n->n = value;
+	left_n->n = value;
 	if (parent->left != NULL)
 	{	
-		parent->left->parent = node_n;
-		node_n->left = parent->left;
+		parent->left->parent = left_n;
+		left_n->left = parent->left;
 	}
-	parent->left = node_n;
-	
-	
-	
-	return (node_n);
+	parent->left = left_n;
+
+
+
+	return (left_n);
 }
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right- ....
+ *@parent: ....
+ *@value: .....
+ *Return: ......
+ **/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_n;
+
+	right_n = binary_tree_node(parent, value);
+
+	if (right_n == NULL || parent == NULL)
+		return (NULL);
+
+	right_n->n = value;
+	if (parent->right != NULL)
+	{	
+		parent->right->parent = right_n;
+		right_n->right = parent->right;
+	}
+	parent->right = right_n;
+
+
+
+	return (right_n);
+}
