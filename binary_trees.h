2b0e3782ffcaf56c195b53373b694e9e3ae2b7ce
jaaferfares <jaafarferes@gmail.com>
2022-07-20 10:59:44 +0100

    test

--- binary_trees.h
+++ binary_trees.h
@@ -2,10 +2,10 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,20 +14,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-
-typedef struct binary_tree_s bst_t;
-void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+/* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -49,4 +48,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
 #endif
