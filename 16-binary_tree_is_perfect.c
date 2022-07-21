de44fd4f19db57bc5eeeba7d36ceb5517800dbd3
jaaferfares <jaafarferes@gmail.com>
2022-07-21 11:28:23 +0100

    still testing

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,20 +1,17 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
- *
- *
+ * binary_tree_is_perfect - ...
+ *@tree: ...
+ *Return: ....
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
+	
 	if (tree == NULL)
 		return (0);
-	if (tree->left == tree->right)
-		return (1);
-	else
+	if (binary_tree_is_perfect(tree->left) != binary_tree_is_perfect(tree->right))
 		return (0);
+	else
+		return (1);
 
 }
